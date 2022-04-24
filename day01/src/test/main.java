dfb7ae1ea185071d10832987a08917ad0689f715
ruyi <housenwen1@gmail.com>
2022-04-24 18:44:14 +0800

    func

--- day01/src/test/main.java
+++ day01/src/test/main.java
@@ -3,5 +3,24 @@ package test;
 public class main {
     public static void main(String[] args) {
         System.out.println("1234");
+
+        test1();
+        test2("zhangsan");
+        String s = test3(100);
+        System.out.println(s);
+
     }
+    public static void test1(){
+        System.out.println("11111");
+    }
+
+    public static void test2(String str){
+        System.out.println(str);
+    }
+
+    public static String test3(Integer i){
+        return i.toString();
+    }
+
+
 }
